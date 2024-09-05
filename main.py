e758695b50b4d09089d1a113cebaa6985221c3a2
Megan Jones <megan.jones@mobilise.cloud>
2024-09-05 09:32:53 +0100

    updated welcome message and docstrings

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class URL(BaseModel):
 
 
 def generate_shortened_url() -> str:
+    """
+    Generates a shortened URL using secrets
+    :return: str
+    """
     while True:
         shortened_path = secrets.token_urlsafe(6)
         shortened_url = f'https://short.url/{shortened_path}'
@@ -22,7 +26,12 @@ def generate_shortened_url() -> str:
             return shortened_url
 
 
-def get_key_from_value(value) -> str | None:
+def get_key_from_value(value: str) -> str | None:
+    """
+    When given a dictionary value, it returns the key belonging to the value
+    :param value: dictionary value
+    :return: str or None
+    """
     for key, val in fake_db.items():
         if val == value:
             return key
@@ -31,6 +40,10 @@ def get_key_from_value(value) -> str | None:
 
 @app.get("/")
 async def root():
+    """
+    Initial URL endpoint
+    :return: JSON
+    """
     return {"message": "Welcome to Megan Jones' URL shortener.",
             "to_shorten": "To shorten a URL, use the endpoint /shorten/<URL>",
             "to_lengthen": "To use a shortened URL, use the endpoint /redirect/<SHORT_URL>"}
@@ -38,14 +51,23 @@ async def root():
 
 @app.get("/shorten/{url:path}")
 async def url_shortener(url: str):
+    """
+    Endpoint to shorten a URL
+    :param url: str
+    :return: URL
+    """
     shortened_url = generate_shortened_url()
     fake_db[shortened_url] = url
-    print(fake_db)
     return URL(url=shortened_url)
 
 
 @app.get('/redirect/{short_url:path}')
 async def redirect_to_longer(short_url: str):
+    """
+    Endpoint to redirect to original URL when shortened url is used
+    :param short_url: str
+    :return: RedirectResponse or Exception
+    """
     path = fake_db[short_url]
     if short_url in fake_db:
         return RedirectResponse(f'/{path}')
@@ -55,6 +77,11 @@ async def redirect_to_longer(short_url: str):
 
 @app.get('/{short_url:path}')
 async def redirect(short_url: str):
+    """
+    Endpoint for original URL after redirection
+    :param short_url: str
+    :return: JSON
+    """
     redirected_path = short_url.replace(f'{LOCAL_HOST}/', "")
     print(redirected_path)
     original_path = get_key_from_value(redirected_path)
